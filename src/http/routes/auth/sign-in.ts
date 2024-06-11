d35e58da3e9cf1007e8fb636ee8d6588ec4b998d
souorichard <elaine.ra96@gmail.com>
2024-06-11 18:00:15 -0300

    feat: add sign-in route

---
+++ src/http/routes/auth/sign-in.ts
@@ -0,0 +1,38 @@
+import { FastifyInstance } from 'fastify'
+import { db } from '../../../db'
+import { z } from 'zod'
+import { generateToken } from '../../authentication'
+
+const signInSchema = z.object({
+  email: z.string().email(),
+})
+
+export async function signIn(app: FastifyInstance) {
+  app.post('/sign-in', async (request, reply) => {
+    try {
+      const { email } = signInSchema.parse(request.body)
+
+      const userExist = await db.user.findUnique({
+        where: {
+          email
+        }
+      })
+
+      if (userExist) {
+        const { id, email } = userExist
+
+        const token = generateToken({ id ,email })
+
+        return reply.status(200).send({ id, token })
+      }
+
+      return reply.status(400).send({
+        message: 'Email not found.'
+      })
+    } catch (err) {
+      return reply.status(500).send({
+        message: 'Internal server error.'
+      })
+    }
+  })
+}
