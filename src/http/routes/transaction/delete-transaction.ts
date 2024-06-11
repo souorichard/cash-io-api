90626538b95943b2121371bdc437bbefa58788f8
souorichard <elaine.ra96@gmail.com>
2024-06-11 19:07:12 -0300

    feat: add delete-transaction route

---
+++ src/http/routes/transaction/delete-transaction.ts
@@ -0,0 +1,47 @@
+import { FastifyInstance } from 'fastify'
+import { isAuthenticated } from '../../authentication'
+import { db } from '../../../db'
+import { z } from 'zod'
+import { decodeToken } from '../../../utils/decode-token'
+
+const deleteTransactionSchema = z.object({
+  id: z.string().cuid(),
+})
+
+export async function deleteTransaction(app: FastifyInstance) {
+  app.delete('/transactions', {
+    preHandler: (request, reply, done) => {
+      isAuthenticated({ request, reply, done })
+    }
+  }, async (request, reply) => {
+    try {
+      const { id } = deleteTransactionSchema.parse(request.body)
+
+      const { id: userId } = await decodeToken(request, reply)
+
+      const verifyTransaction = await db.transaction.findUnique({
+        where: {
+          id,
+          createdById: userId
+        }
+      })
+
+      if (!verifyTransaction) {
+        return reply.status(404).send({ message: 'Transaction not found.' })
+      }
+
+      await db.transaction.delete({
+        where: {
+          id,
+          createdById: userId
+        }
+      })
+
+      return reply.status(200).send({ message: 'Transaction sucessfully deleted.' })
+    } catch (err) {
+      return reply.status(500).send({
+        message: 'Internal server error.'
+      })
+    }
+  })
+}
